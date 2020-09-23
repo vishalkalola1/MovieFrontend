20f4bdd2c2fed1f120058c156b74f2ce8015d6df
vkalola <vkalola@hexaglobe.com>
2020-09-23 17:57:36 +0200

    Socker Comment Implement

--- src/Views/DetailsMovie.js
+++ src/Views/DetailsMovie.js
@@ -8,35 +8,49 @@ import SocketIOClient from 'socket.io-client';
 class DetailsMovie extends Component {
 
     idMovie = -1
-
     constructor(props) {
         super(props)
         this.idMovie = props.match.params.idMovie
         this.state = {
             dataMovie: {},
-            rating : 0.0
+            rating: 0.0,
+            comment: "",
+            comments: [],
+            socket: io('http://localhost:3001', { query: `movieId=${this.idMovie}` })
         }
+
         this.submitComment = this.submitComment.bind(this)
         this.changeRating = this.changeRating.bind(this)
-        this.socket =  io('http://localhost:3001',{query:`movieId=${this.idMovie}`});
+        this.handleInputChange = this.handleInputChange.bind(this);
 
-        this.socket.on('connect', function(){
+        this.state.socket.on('connect', ()=> {
             console.log("Socket Connected")
+            
         });
-    
-        this.socket.on('getComment', function(data){
-    
-        });
-        this.socket.on('disconnect', function(){
+
+
+        this.state.socket.on('disconnect',()=> {
             console.log("Socket Disconnected")
         });
+
+        this.state.socket.on(`getComment${this.idMovie}`,(data)=> {
+            var proto = Object.getPrototypeOf(data);
+            if (proto === [].prototype) {
+                this.setState({comments:data})
+            } else {
+                this.setState({comments:[...this.state.comments,data]})
+            }
+        })
+
+        
     }
+
 
     componentDidMount() {
-        this.getDetails()
+        // this.getDetails()
     }
 
-    returnHome(){
+    returnHome() {
         this.props.history.push('/')
     }
 
@@ -49,7 +63,7 @@ class DetailsMovie extends Component {
             } else {
                 throw data
             }
-        }).then((data)=>{
+        }).then((data) => {
             console.log(data)
             this.setState({
                 dataMovie: data
@@ -59,21 +73,39 @@ class DetailsMovie extends Component {
         })
     }
 
-    returnData(data){
-        console.log(data)
+    returnData(data) {
+        debugger;
+        this.setState({"comment":""})
     }
 
-    submitComment(){
-        let json = {"name":"vishal"}
-        this.socket.emit("addcomment",json,this.returnData);
-
-    }
-
-    changeRating( newRating ) {
-        this.setState({
-          rating: newRating
+    submitComment() {
+        let json = {
+            "title": "vishal",
+            "comment": this.state.comment,
+            "userid": "1",
+            "movieid": this.idMovie
+        }
+        this.state.socket.emit("addcomment", json, (data)=> {
+            this.returnData(data)
         });
-      }
+
+    }
+
+    changeRating(newRating) {
+        this.setState({
+            rating: newRating
+        });
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
 
     isEmpty(obj) {
         return Object.keys(obj).length === 0;
@@ -81,28 +113,28 @@ class DetailsMovie extends Component {
 
     renderdetails() {
         return (
-            <div id="wrapperdetails"> 
-                <div id="c1details" style={{color:'white'}}>
-                    <br/>
-                    <h1 style={{textShadow: '2px 2px 5px red'}}>{this.state.dataMovie.title}</h1>
+            <div id="wrapperdetails">
+                <div id="c1details" style={{ color: 'white' }}>
+                    <br />
+                    <h1 style={{ textShadow: '2px 2px 5px red' }}>{this.state.dataMovie.title}</h1>
                     <img class="poster" src={this.state.dataMovie.imageLink} />
-                    <div style={{marginLeft:'5%', marginTop:'5%'}}>
-                        
-                        <div style={{marginBottom:'20px'}}>
+                    <div style={{ marginLeft: '5%', marginTop: '5%' }}>
+
+                        <div style={{ marginBottom: '20px' }}>
                             <h5><b>Details: </b></h5><h6>{this.state.dataMovie.details}</h6>
                         </div>
-                        <div style={{marginBottom:'20px'}}>
-                            <h5 style={{display: 'inline-block'}}><b> Release Date: </b></h5> <h6 style={{display: 'inline-block'}}>{this.state.dataMovie.releaseDate}</h6>
+                        <div style={{ marginBottom: '20px' }}>
+                            <h5 style={{ display: 'inline-block' }}><b> Release Date: </b></h5> <h6 style={{ display: 'inline-block' }}>{this.state.dataMovie.releaseDate}</h6>
                         </div>
 
-                        <div style={{marginBottom:'20px'}}>
-                            <h5 style={{display: 'inline-block'}}><b>  Movie Type:  </b></h5> <h6 style={{display: 'inline-block'}}>{this.state.dataMovie.category}</h6>
+                        <div style={{ marginBottom: '20px' }}>
+                            <h5 style={{ display: 'inline-block' }}><b>  Movie Type:  </b></h5> <h6 style={{ display: 'inline-block' }}>{this.state.dataMovie.category}</h6>
                         </div>
-                        <div style={{marginBottom:'20px'}}>
-                            <h5 style={{display: 'inline-block'}}><b>  Movie Director:  </b></h5> <h6 style={{display: 'inline-block'}}>{this.state.dataMovie.movieDirector}</h6>
+                        <div style={{ marginBottom: '20px' }}>
+                            <h5 style={{ display: 'inline-block' }}><b>  Movie Director:  </b></h5> <h6 style={{ display: 'inline-block' }}>{this.state.dataMovie.movieDirector}</h6>
                         </div>
                     </div>
-                    <div style={{marginLeft:'30px', marginRight:'30px'}}>
+                    <div style={{ marginLeft: '30px', marginRight: '30px' }}>
                         <span class="heading"><b>User Rating</b></span>
                         <StarRatings
                             rating={this.state.rating}
@@ -113,7 +145,7 @@ class DetailsMovie extends Component {
                             name='rating'
                         />
                         <p>4.1 average based on 254 reviews.</p>
-                        <hr style={{ border:'3px solid #f1f1f1' }} />
+                        <hr style={{ border: '3px solid #f1f1f1' }} />
 
                         <div class="row" >
                             <div class="side">
@@ -169,107 +201,41 @@ class DetailsMovie extends Component {
                                 </div>
                             </div>
                             <div class="side right">
-                            <div>20</div>
-                        </div>
+                                <div>20</div>
+                            </div>
                         </div>
                     </div>
-                    <br/>
-                </div> 
-                  
-                <div id="c2details" style={{ color: "white"}}>
-                    <br/>
-                    <h1 style={{textShadow: '2px 2px 5px red'}}>Comments</h1>
-                    <div id="wrapperdetails" style={{marginLeft:'10%'}}>
+                    <br />
+                </div>
+
+                <div id="c2details" style={{ color: "white" }}>
+                    <br />
+                    <h1 style={{ textShadow: '2px 2px 5px red' }}>Comments</h1>
+                    <div id="wrapperdetails" style={{ marginLeft: '10%' }}>
                         <div id="cleftsearch">
-                            <input class="oneSub" type="text" value={this.state.search} name="search" placeholder="Write comment" required style={{ height:'55px' }}  onChange={this.handleInputChange} required/>
+                            <input class="oneSub" type="text" value={this.state.comment} name="comment" placeholder="Write comment" style={{ height: '55px' }} onChange={this.handleInputChange} required />
                         </div>
                         <div id="crightsearch">
                             <button onClick={this.submitComment}>Submit</button>
                         </div>
                     </div>
                     <div class="CommentList">
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
-
-                        <div class="commentbubble">
-                            <div style={{height:'15px'}}></div>
-                            <p>Vishal</p>
-                            <p>
-                            sdfhskdhfksdjfshfjhfshfkjsdhfkhfsf fdjksdfbsdf sdfjksfbsdf sdfkbsdfsbdf sfksfsfbsbfsf skfbskjfbskf skdfbsjkdfbsfk skdfbjsbfkjsdbfksdf sfkbsfkjsbdfksdbjfksf  fksdjfbskjfbskfbksdbfks skfjsdkjbfksjbfkjsf sfkjdsbfbsdfsdjkbf skfbsdfsdkfbskfbsdjfs fskfbskjbbfsk
-                            </p>
-                            <div style={{height:'1px'}}></div>
-                        </div>
+                        { this.state.comments.length > 0 ? this.renderCommentBubble() : <div>No Comments </div> }
                     </div>
                 </div>
+            </div>
+        )
+    }
+
+    renderCommentBubble() {
+        return this.state.comments.map((comment) =>
+            <div class="commentbubble">
+                <div style={{ height: '15px' }}></div>
+                    <p>{comment.title}</p>
+                    <p>
+                        {comment.comment}
+                    </p>
+                <div style={{ height: '1px' }}></div>
             </div>
         )
     }
